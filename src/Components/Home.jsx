f7fd9a3790414074f7798f0c97df2ee8b69252b3
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-08-16 20:45:49 +0600

    Complete Pagination by backend

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,20 +4,67 @@ import axios from "axios";
 
 const Home = () => {
   const [datas, setDatas] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const limit = 9;
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get("http://localhost:3000/product");
-      console.log(data);
-      setDatas(data);
+      const { data } = await axios.get(
+        `http://localhost:3000/product?page=${currentPage}&limit=${limit}`
+      );
+      setDatas(data.data);
+      setTotalPages(data.totalPages);
     };
     getData();
-  }, []);
+  }, [currentPage]);
+
+  const handlePageChange = (newPage) => {
+    if (newPage > 0 && newPage <= totalPages) {
+      setCurrentPage(newPage);
+    }
+  };
+
+  const renderPaginationButtons = () => {
+    const buttons = [];
+    for (let i = 1; i <= totalPages; i++) {
+      buttons.push(
+        <button
+          key={i}
+          onClick={() => handlePageChange(i)}
+          className={`join-item btn ${i === currentPage ? "btn-active" : ""}`}
+        >
+          {i}
+        </button>
+      );
+    }
+    return buttons;
+  };
+
   return (
-    <div className="my-10 grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {datas.map((data, index) => (
-        <SingleData key={index} data={data}></SingleData>
-      ))}
+    <div className="my-10">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {datas.map((data, index) => (
+          <SingleData key={index} data={data}></SingleData>
+        ))}
+      </div>
+      <div className="flex justify-center items-center mt-6">
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className="btn btn-primary mr-2"
+        >
+          Previous
+        </button>
+        {renderPaginationButtons()}
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          className="btn btn-primary ml-2"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
