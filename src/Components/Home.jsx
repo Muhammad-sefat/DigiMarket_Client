70d5b5c44f5c0808e2facae62a4efddb8c6d28ec
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-08-16 15:40:34 +0600

    Complete card design with all data

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,7 +4,7 @@ import SingleData from "./SingleData";
 
 const Home = () => {
   return (
-    <div className="my-8 grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+    <div className="my-10 grid md:grid-cols-2 lg:grid-cols-3 gap-6">
       {datas.map((data, index) => (
         <SingleData key={index} data={data}></SingleData>
       ))}
--- src/Components/SingleData.jsx
+++ src/Components/SingleData.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import Rating from "react-rating-stars-component";
 
 const SingleData = ({ data }) => {
-  console.log(data.image);
   return (
-    <div>
-      <div className="card bg-base-100 shadow-xl h-full">
+    <div className="my-8">
+      <div className="card bg-base-100 shadow-xl h-full transform hover:scale-105 transition-transform duration-300 ease-in-out">
         <figure>
           <img src={data.image} alt="Shoes" />
         </figure>
         <div className="card-body text-left">
-          <h2 className="card-title">{data.name}</h2>
-          <p>{data.description}</p>
-          <p>Price : ${data.price}</p>
-          <p>Brand : {data.brand}</p>
-          <p>Date : {data.createdAt}</p>
+          <Rating
+            count={5}
+            value={data.rating}
+            size={24}
+            activeColor="#ffd700"
+            edit={false}
+          />
+          <h2 className="card-title text-2xl">{data.name}</h2>
+          <p className="text-gray-600">{data.description}</p>
+          <p className="text-base font-semibold">
+            <span className="text-lg">Price</span> : ${data.price}
+          </p>
+          <p className="text-base font-semibold">
+            <span className="text-lg">Brand</span> : {data.brand}
+          </p>
+          <p className="text-base font-semibold">
+            <span className="text-lg">Date </span>: {data.createdAt}
+          </p>
         </div>
       </div>
     </div>
